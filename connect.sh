c2f8123155e191a18e0e0090f77c648872ddee73
Nils Wieber <wiebernils@googlemail.com>
2020-05-12 15:37:55 +0200

    Fixed bug where commands like sh -c '...' where not interpreted correctly

--- connect.sh
+++ connect.sh
@@ -1,5 +1,5 @@
-#! /bin/sh
+#! /bin/bash
 cd ~/
 echo $CONNECT_SSH_KEY | base64 -d  > id_rsa
 chmod 600 id_rsa
-ssh -o "StrictHostKeyChecking=no" -i id_rsa $CONNECT_SSH_TARGET $@
+ssh -t -o "StrictHostKeyChecking=no" -i id_rsa $CONNECT_SSH_TARGET "$(printf '%q ' "$@")"
